9713fb33ef83a3fd91c6f556a9614cce69ae001e
anishmandal1617 <basketb523@gmail.com>
2024-09-20 01:11:11 +0545

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+﻿using CareerManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CareerManagement.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+        {
+            return await _context.Todos.ToListAsync();
+        }
+    }
+}
